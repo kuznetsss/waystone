bd6163a8b5e0174b89f6800dba6a8bff62fa0d77
Sergey Kuznetsov <kuzzz99@gmail.com>
2024-10-28 00:14:12 +0000

    feat: Add basic config (#5)

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+
+mod config;
+
+/// Waystone: Load balancer and rate limiter supporting HTTP and WebSocket
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CliArgs {
+    /// Config path
+    #[arg(short, long, default_value="./config.yaml")]
+    config: String,
+}
+
+fn main() {
+    let cliArgs = CliArgs::parse();
+    dbg!(cliArgs.config);
+}
